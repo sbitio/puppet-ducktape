87c8a34c2e68353a29478e59311a223b968b974f
Pedro González Serrano <niteman.es@gmail.com>
2014-09-13 13:44:09 +0200

    use explicit ducktape hiera keys

--- manifests/haproxy/autoload.pp
+++ manifests/haproxy/autoload.pp
@@ -14,13 +14,13 @@ class ducktape::haproxy::autoload (
   }
 
   if $load_frontends {
-    $haproxy_frontend_defaults = hiera('haproxy::frontend::defaults', {})
-    create_resources('haproxy::frontend', hiera_hash('haproxy::frontends'), $haproxy_frontend_defaults)
+    $haproxy_frontend_defaults = hiera('ducktape::haproxy::frontend::defaults', {})
+    create_resources('haproxy::frontend', hiera_hash('ducktape::haproxy::frontends', {}), $haproxy_frontend_defaults)
   }
 
   if $load_backends {
-    $haproxy_backend_defaults = hiera('haproxy::backend::defaults', {})
-    create_resources('haproxy::backend', hiera_hash('haproxy::backends'), $haproxy_backend_defaults)
+    $haproxy_backend_defaults = hiera('ducktape::haproxy::backend::defaults', {})
+    create_resources('haproxy::backend', hiera_hash('ducktape::haproxy::backends', {}), $haproxy_backend_defaults)
   }
 
 }
--- manifests/newrelic.pp
+++ manifests/newrelic.pp
@@ -5,9 +5,9 @@ class ducktape::newrelic (
   validate_bool($enabled)
 
   if $enabled {
-    create_resources('newrelic::server', hiera_hash('newrelic::server',{}))
+    create_resources('newrelic::server', hiera_hash('ducktape::newrelic::server', {}))
     if defined('::php') and defined(Class['::php']) {
-      create_resources('newrelic::php', hiera_hash('newrelic::php',{}))
+      create_resources('newrelic::php', hiera_hash('ducktape::newrelic::php',{}))
     }
   }
 
