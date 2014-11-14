9afcbbb217cd80de54d3073ec0520aa5f84da6c8
Pedro González Serrano <niteman.es@gmail.com>
2014-11-14 14:10:18 +0100

    haproxy: added rsyslog config

--- manifests/haproxy.pp
+++ manifests/haproxy.pp
@@ -7,12 +7,15 @@ class ducktape::haproxy (
   if $enabled {
     include ducktape::haproxy::autoload
 
-    # External checks.
+    # External configs.
     if defined('::munin::node') and defined(Class['::munin::node']) {
       include ::ducktape::haproxy::external::munin_node_plugin
     }
     if defined('::monit') and defined(Class['::monit']) {
       include ::ducktape::haproxy::external::monit
+    }
+    if defined('::rsyslog') and defined(Class['::rsyslog']) {
+      include ::ducktape::haproxy::external::rsyslog
     }
   }
 
---
+++ manifests/haproxy/external/rsyslog.pp
@@ -0,0 +1,21 @@
+class haproxy::external::rsyslog (
+  $ensure      = present,
+  $enabled     = true,
+  $priority    = '49',
+  $udp_listen  = true,
+  $udp_ip      = '127.0.0.1',
+  $udp_port    = '514',
+  $programname = 'haproxy',
+  $log_dst     = '/var/log/haproxy.log',
+) {
+
+  #TODO# Add validations
+
+  if $enabled {
+    rsyslog::snippet { "${priority}-${programname}" :
+      ensure  => $ensure,
+      content => template('ducktape/haproxy/external/rsyslog.conf.erb'),
+    }
+  }
+
+}
