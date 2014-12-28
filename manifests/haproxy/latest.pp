e0ab2524c3241880ad8b784e8da61cebaf971fa0
Pedro González Serrano <niteman.es@gmail.com>
2014-12-28 17:52:47 +0100

    apt class is required to use apt::ppa

--- manifests/haproxy/latest.pp
+++ manifests/haproxy/latest.pp
@@ -10,6 +10,7 @@ class ducktape::haproxy::latest (
         case $::lsbdistcodename {
           'trusty' : {
             # Based on http://haproxy.debian.net/
+            include apt
             apt::ppa { 'ppa:vbernat/haproxy-1.5': }
             -> Class['::haproxy::install']
           }
