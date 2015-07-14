168056f81fc35f466a6afb40468e3e9cbfcc296f
Pedro González Serrano <niteman.es@gmail.com>
2015-07-14 18:58:50 +0200

    change monit service check mode on Debien since no systemd file is provided within packages, take 2

--- manifests/mysql/external/monit.pp
+++ manifests/mysql/external/monit.pp
@@ -5,6 +5,17 @@ class ducktape::mysql::external::monit(
   validate_bool($enabled)
 
   if $enabled {
+    $init_system = $::operatingsystem ? {
+      'Ubuntu' => $lsbmajdistrelease ? {
+        /(12\.|14\.)/ => 'sysv',
+        default       => undef,
+      },
+      'Debian' => $lsbdistcodename ? {
+        'jessie' => 'sysv',
+        default  => undef,
+      },
+      default  => undef,
+    }
     $connection_test = {
       type   => 'connection',
       host   => '127.0.0.1',
@@ -12,8 +23,9 @@ class ducktape::mysql::external::monit(
       action => 'restart',
     }
     monit::check::service { "$::mysql::server::service_name":
-      pidfile => $::mysql::params::pidfile,
-      binary  => $::osfamily ? {
+      init_system => $init_system,
+      pidfile     => $::mysql::params::pidfile,
+      binary      => $::osfamily ? {
         'Debian' => '/usr/sbin/mysqld',
         'Redhat' => $::lsbmajdistrelease ? {
           7       => '/usr/libexec/mysqld',
@@ -21,7 +33,7 @@ class ducktape::mysql::external::monit(
         },
         default  => undef,
       },
-      tests   => [$connection_test, ],
+      tests       => [$connection_test, ],
       #TODO# if 5 restarts within 5 cycles then timeout
     }
   }
