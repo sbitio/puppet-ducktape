fbe3544a33d26dece8b6eb8ee0b5cd0ce5a24b01
Pedro González Serrano <niteman.es@gmail.com>
2015-09-01 11:46:55 +0200

    logstash/monit: workaround init script start bug

--- manifests/logstash/external/monit.pp
+++ manifests/logstash/external/monit.pp
@@ -14,11 +14,22 @@ class ducktape::logstash::external::monit(
       default  => undef,
     }
 
+    #TODO# Reviewme
+    # As 20150901 logstash start via init script fails inconditionally when pid file exists in Jessie (only tested platform)
+    $program_start = $::operatingsystem ? {
+      'Debian' => $::lsbdistcodename ? {
+        'jessie' => '/etc/init.d/logstash restart',
+        default  => undef,
+      },
+      default  => undef,
+    }
+
     monit::check::service { 'logstash':
-      init_system => $init_system,
-      pidfile     => '/var/run/logstash.pid',
-      binary      => '/usr/bin/java',
-      tests       => $ducktape::logstash::external::monit::tests,
+      init_system   => $init_system,
+      program_start => $program_start,
+      pidfile       => '/var/run/logstash.pid',
+      binary        => '/usr/bin/java',
+      tests         => $ducktape::logstash::external::monit::tests,
       #TODO# if 5 restarts within 5 cycles then timeout
     }
   }
