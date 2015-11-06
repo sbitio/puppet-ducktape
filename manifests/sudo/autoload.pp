cf93b586b269d76377d37944700fca43da387e90
Pedro González Serrano <niteman.es@gmail.com>
2015-11-06 10:20:52 +0100

    sudo::autload: default to hiera_hash autoloading

--- manifests/sudo/autoload.pp
+++ manifests/sudo/autoload.pp
@@ -1,12 +1,18 @@
 class ducktape::sudo::autoload (
-  $autoload = true,
+  $autoload             = true,
+  $hiera_merge_strategy = hiera_hash,
 ) {
 
   validate_bool($autoload)
 
   if $autoload {
     $conf_defaults = hiera('ducktape::sudo::conf::defaults', {})
-    $confs         = hiera('ducktape::sudo::confs', {})
+    if ($hiera_merge_strategy == 'hiera_hash') {
+      $confs = hiera_hash('ducktape::sudo::confs', {})
+    }
+    else {
+      $confs = hiera('ducktape::sudo::confs', {})
+    }
     create_resources('::sudo::conf', $confs, $conf_defaults)
   }
 
