eee6dcefd32715a74dee29461ca37cc695e63f2d
Pedro González Serrano <niteman.es@gmail.com>
2016-06-17 12:43:50 +0200

    filebeat,topbeat: fix monit tests, take2

--- manifests/topbeat/external/monit.pp
+++ manifests/topbeat/external/monit.pp
@@ -6,7 +6,7 @@ class ducktape::topbeat::external::monit(
   #TODO# Add more validations
 
   if $enabled {
-    $binary  = '/usr/bin/topbeat-god',
+    $binary = '/usr/bin/topbeat-god'
     monit::check::service { 'topbeat':
       matching => $binary,
       binary   => $binary,
