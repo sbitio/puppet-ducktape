938dce7994a2785a98819a594916d9eddbf205fe
sergio <sergio@wwb.cc>
2016-09-22 15:24:22 +0200

    monit-slapd  slapd_service_file path changed to sysv in jessie

--- manifests/openldap/external/monit.pp
+++ manifests/openldap/external/monit.pp
@@ -9,8 +9,12 @@ class ducktape::openldap::external::monit(
     $pidfile = $::osfamily ? {
       'Debian' => '/var/run/slapd/slapd.pid',
     }
+    $init_system = $::lsbdistcodename ? {
+      'jessie' => 'sysv',
+    }
     monit::check::service { $::openldap::server:service:
       pidfile => $pidfile,
+      init_system => $init_system,
     }
   }
 
