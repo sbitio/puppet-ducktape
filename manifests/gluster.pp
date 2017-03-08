92954fe5a87bfa5ae9951719ba9cb19a4b288859
Jonathan Araña Cruz <jonhattan@faita.net>
2017-03-08 08:37:02 +0000

    Add gluster apt key.

--- manifests/gluster.pp
+++ manifests/gluster.pp
@@ -8,5 +8,15 @@ class ducktape::gluster (
     include ducktape::gluster::autoload
   }
 
+  case $::osfamily {
+    'Debian': {
+      apt::key { 'gluster':
+        key        => "A4703C37D3F4DE7F1819E980FE79BB52D5DC52DC",
+        key_server => "keys.gnupg.net",
+        notify     => Class['apt::update'],
+      }
+    }
+  }
+
 }
 
