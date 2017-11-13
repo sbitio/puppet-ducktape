9a44699bbfbb49ac1ce7feb8c4253cfe3704a3e0
Jonathan Araña Cruz <jonhattan@faita.net>
2017-11-13 18:41:37 +0100

    Newrelic/monit: Provide explicit commands for start/stop actions.

--- manifests/newrelic/external/monit.pp
+++ manifests/newrelic/external/monit.pp
@@ -10,9 +10,11 @@ class ducktape::newrelic::external::monit(
     $pidfile_nrsysmond = '/var/run/newrelic/nrsysmond.pid'
     $init_system = 'sysv'
     monit::check::service { 'newrelic-daemon':
-      init_system => $init_system,
-      pidfile     => $pidfile_daemon,
-      binary      => '/usr/bin/newrelic-daemon',
+      init_system   => $init_system,
+      pidfile       => $pidfile_daemon,
+      binary        => '/usr/bin/newrelic-daemon',
+      program_start => '/etc/init.d/newrelic-daemon start',
+      program_stop  => '/etc/init.d/newrelic-daemon stop',
     }
     monit::check::service { 'newrelic-sysmond':
       init_system => $init_system,
