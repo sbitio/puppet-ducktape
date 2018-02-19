70c9eb12bdb764dd57b3adcbc682d8dbb28bd865
Pedro González Serrano <niteman.es@gmail.com>
2018-02-19 10:27:43 +0100

    Fix logstash monit test on 6.x

--- manifests/logstash/external/monit.pp
+++ manifests/logstash/external/monit.pp
@@ -7,30 +7,11 @@ class ducktape::logstash::external::monit(
 
   if $enabled {
 
-    $init_system = $::operatingsystem ? {
-      'Debian' => $::lsbdistcodename ? {
-        'jessie' => 'sysv',
-        default  => undef,
-      },
-      default  => undef,
-    }
-
-    #TODO# Reviewme
-    # As 20150901 logstash start via init script fails inconditionally when pid file exists in Jessie (only tested platform)
-    $program_start = $::operatingsystem ? {
-      'Debian' => $::lsbdistcodename ? {
-        'jessie' => '/etc/init.d/logstash restart',
-        default  => undef,
-      },
-      default  => undef,
-    }
-
     monit::check::service { 'logstash':
-      init_system   => $init_system,
-      program_start => $program_start,
-      pidfile       => '/var/run/logstash.pid',
-      binary        => '/usr/bin/java',
-      tests         => $tests,
+      systemd_file => '/etc/systemd/system/logstash.service',
+      pidfile      => '/var/run/logstash.pid',
+      binary       => '/usr/bin/java',
+      tests        => $tests,
       #TODO# if 5 restarts within 5 cycles then timeout
     }
   }
