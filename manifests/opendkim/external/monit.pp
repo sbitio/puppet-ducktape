ba07728cedd76558a0c0492a5c69bc310577bf8f
Pedro González Serrano <niteman.es@gmail.com>
2018-02-19 10:28:05 +0100

    Fix opendkin monit test on Debian jessie

--- manifests/opendkim/external/monit.pp
+++ manifests/opendkim/external/monit.pp
@@ -6,16 +6,24 @@ class ducktape::opendkim::external::monit(
   validate_bool($enabled)
 
   if $enabled {
+    $init_system = $::operatingsystem ? {
+      'Debian' => $::lsbdistcodename ? {
+        'jessie' => 'sysv',
+        default  => undef,
+      },
+      default  => undef,
+    }
+
     $pidfile = $::osfamily ? {
       /(RedHat|Debian)/ => '/var/run/opendkim/opendkim.pid',
     }
     $test = {
       type     => connection,
-      protocol => smtp,
       port     => $port,
       action   => 'restart',
     }
     monit::check::service { 'opendkim':
+      init_system => $init_system,
       pidfile     => $pidfile,
       tests       => [$test,]
     }
