c9c273d45a47639492a536447d279aa1001441ab
Pedro González Serrano <niteman.es@gmail.com>
2018-04-05 21:27:37 +0200

    firewall: Add support for firewall(chain)? purging

--- manifests/firewall.pp
+++ manifests/firewall.pp
@@ -1,10 +1,22 @@
 class ducktape::firewall (
   $enabled = true,
+  $purge_firewall = true,
+  $purge_firewallchain = true,
 ) {
 
   validate_bool($enabled)
+  validate_bool($purge_firewall)
+  validate_bool($purge_firewallchain)
 
   if $enabled {
+    resources { 'firewall':
+      purge => $purge_firewall,
+    }
+
+    resources { 'firewallchain':
+      purge => $purge_firewallchain,
+    }
+
     include ducktape::firewall::autoload
   }
 
