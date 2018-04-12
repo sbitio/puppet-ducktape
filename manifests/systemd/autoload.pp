4ee0cc19e29bcd86efd1d7493fa82572558c9e66
Rodrigo Alfaro <rodricels@users.noreply.github.com>
2018-04-12 12:04:25 +0200

    camptocamp/systemd: add unit files

--- manifests/systemd/autoload.pp
+++ manifests/systemd/autoload.pp
@@ -1,12 +1,19 @@
 class ducktape::systemd::autoload (
   $load_dropin_files = true,
+  $load_unit_files = true,
 ) {
 
   validate_bool($load_dropin_files)
+  validate_bool($load_unit_files)
 
   if $load_dropin_files {
     $systemd_dropin_files_defaults = hiera('ducktape::systemd::dropin_files::defaults', {})
     create_resources('::systemd::dropin_file', hiera_hash('ducktape::systemd::dropin_files', {}), $systemd_dropin_files_defaults)
+  }
+
+  if $load_unit_files {
+    $systemd_unit_files_defaults = hiera('ducktape::systemd::unit_files::defaults', {})
+    create_resources('::systemd::unit_file', hiera_hash('ducktape::systemd::unit_files', {}), $systemd_unit_files_defaults)
   }
 
 }
