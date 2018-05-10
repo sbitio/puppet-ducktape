2f067a603fb658ef8890e3caa4aa53ce0637da14
Jonathan Araña Cruz <jonhattan@faita.net>
2018-05-10 16:39:27 +0200

    Anchors - fix syntax error.

--- manifests/anchors.pp
+++ manifests/anchors.pp
@@ -18,13 +18,13 @@ class ducktape::anchors(
         -> Class[$after['title']]
         -> anchor{ "${anchor_name}_last": }
       }
-      elseif ($before['type'] == 'class') and ($after['type'] != 'class') {
+      elsif ($before['type'] == 'class') and ($after['type'] != 'class') {
         anchor{ "${anchor_name}_first": }
         -> Class[$before['title']]
         -> Resource[$after['type'], $after['title']]
         -> anchor{ "${anchor_name}_last": }
       }
-      elseif ($before['type'] != 'class') and ($after['type'] == 'class') {
+      elsif ($before['type'] != 'class') and ($after['type'] == 'class') {
         anchor{ "${anchor_name}_first": }
         -> Resource[$before['type'], $before['title']]
         -> Class[$after['title']]
