d2ad538cabb5867bd5c0736a10ea98c0c144e46a
Jonathan Araña Cruz <jonhattan@faita.net>
2018-05-10 10:56:22 +0200

    Add support for anchors.

---
+++ manifests/anchors.pp
@@ -0,0 +1,42 @@
+class ducktape::anchors(
+  $enabled = true,
+) {
+
+  if $enabled {
+
+    $anchor_defaults = hiera('ducktape::anchor::defaults', {})
+    $anchors = hiera_hash('ducktape::anchors', {})
+
+    $anchors.each |$anchor_name, $_config| {
+      $config = merge($anchor_defaults, $_config)
+      $before = $config['before'];
+      $after = $config['after'];
+
+      if ($before['type'] == 'class') and ($after['type'] == 'class') {
+        anchor{ "${anchor_name}_first": }
+        -> Class[$before['title']]
+        -> Class[$after['title']]
+        -> anchor{ "${anchor_name}_last": }
+      }
+      elseif ($before['type'] == 'class') and ($after['type'] != 'class') {
+        anchor{ "${anchor_name}_first": }
+        -> Class[$before['title']]
+        -> Resource[$after['type'], $after['title']]
+        -> anchor{ "${anchor_name}_last": }
+      }
+      elseif ($before['type'] != 'class') and ($after['type'] == 'class') {
+        anchor{ "${anchor_name}_first": }
+        -> Resource[$before['type'], $before['title']]
+        -> Class[$after['title']]
+        -> anchor{ "${anchor_name}_last": }
+      }
+      else {
+        anchor{ "${anchor_name}_first": }
+        -> Resource[$before['type'], $before['title']]
+        -> Resource[$after['type'], $after['title']]
+        -> anchor{ "${anchor_name}_last": }
+      }
+    }
+  }
+
+}
