8413a146cc02581fb4cad4896adb6d9ede4c2946
Jonathan Araña Cruz <jonhattan@faita.net>
2018-07-31 18:15:09 +0200

    Logstash: update monit tests for recent releases (> 5.x).

--- manifests/logstash/external/monit.pp
+++ manifests/logstash/external/monit.pp
@@ -1,6 +1,6 @@
 class ducktape::logstash::external::monit(
-  $enabled = true,
-  $tests = [],
+  $enabled  = true,
+  $tests    = [],
 ) {
 
   validate_bool($enabled)
@@ -8,9 +8,9 @@ class ducktape::logstash::external::monit(
   if $enabled {
 
     monit::check::service { 'logstash':
-      systemd_file => '/etc/systemd/system/logstash.service',
-      pidfile      => '/var/run/logstash.pid',
       binary       => '/usr/bin/java',
+      matching     => 'org.logstash.Logstash',
+      systemd_file => '/etc/systemd/system/logstash.service',
       tests        => $tests,
       #TODO# if 5 restarts within 5 cycles then timeout
     }
