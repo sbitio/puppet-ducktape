b99f8bcca79d910c93d7032407d11fbf03ab7f5d
Jonathan Araña Cruz <jonhattan@faita.net>
2018-07-31 17:20:54 +0200

    rsyslog: fix monit check in debian stretch.

--- manifests/rsyslog/external/monit.pp
+++ manifests/rsyslog/external/monit.pp
@@ -5,11 +5,6 @@ class ducktape::rsyslog::external::monit(
   validate_bool($enabled)
 
   if $enabled {
-    $pidfile = $::osfamily ? {
-      'Debian' => '/var/run/rsyslogd.pid',
-      'RedHat' => '/var/run/syslogd.pid',
-    }
-
     $binary = $::osfamily ? {
       'RedHat' => $::lsbmajdistrelease ? {
         6       => '/sbin/rsyslogd',
@@ -17,10 +12,25 @@ class ducktape::rsyslog::external::monit(
       },
       default => '/usr/sbin/rsyslogd',
     }
+    $matching = $::osfamily ? {
+      'Debian' => (versioncmp($::operatingsystemrelease, '9') >= 0) ? {
+        true  => '/usr/sbin/rsyslogd',
+        false => undef,
+      },
+      default => undef,
+    }
+    $pidfile = $::osfamily ? {
+      'Debian' => (versioncmp($::operatingsystemrelease, '9') >= 0) ? {
+        true  => undef,
+        false => '/var/run/rsyslogd.pid',
+      },
+      'RedHat' => '/var/run/syslogd.pid',
+    }
 
     monit::check::service { 'rsyslog':
-      pidfile  => $pidfile,
       binary   => $binary,
+      matching => $matching,
+      pidfile  => $pidfile,
     }
   }
 
