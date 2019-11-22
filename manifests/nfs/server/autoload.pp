dc068dac6ebc3e71ceefc6768e6c2fc2c968d5c8
Pedro González Serrano <niteman.es@gmail.com>
2019-11-22 18:18:05 +0100

    nfs::server: Use lookup insted of hiera for getting nfs_server_export_defaults

--- manifests/nfs/server/autoload.pp
+++ manifests/nfs/server/autoload.pp
@@ -5,7 +5,7 @@ class ducktape::nfs::server::autoload (
   validate_bool($load_exports)
 
   if $load_exports {
-    $nfs_server_export_defaults = hiera('ducktape::nfs::server::export::defaults', {})
+    $nfs_server_export_defaults = lookup('ducktape::nfs::server::export::defaults', {'default_value' => {}})
     create_resources('nfs::server::export', hiera_hash('ducktape::nfs::server::exports', {}), $nfs_server_export_defaults)
   }
 
