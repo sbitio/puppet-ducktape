a490734d90195927722577c0f31207e2ed2ea48a
Pedro González Serrano <niteman.es@gmail.com>
2020-11-12 09:03:12 +0100

    twemproxy: Add external monit test

--- manifests/twemproxy.pp
+++ manifests/twemproxy.pp
@@ -7,7 +7,9 @@ class ducktape::twemproxy (
     if defined('::munin::node') and defined(Class['::munin::node']) {
       include ::ducktape::twemproxy::external::munin_node_plugin
     }
+    if defined('::monit') and defined(Class['::monit']) {
+      include ::ducktape::twemproxy::external::monit
+    }
   }
 
 }
-
