b8714837e51404fcebc54801bfce3fc8efc51663
kris.magjsitari <kris@sbit.io>
2021-09-20 15:54:57 +0200

    haproxy: http-edge redirects

--- manifests/haproxy/autoload.pp
+++ manifests/haproxy/autoload.pp
@@ -10,10 +10,36 @@ class ducktape::haproxy::autoload (
   Boolean $userlist           = true,
   Hash    $userlist_defaults  = {},
   Hash    $userlists          = {},
+
+  Boolean $http_edge          = true,
+  String  $http_edge_frontend = 'default_fe',
+  String  $http_edge_path     = '/opt/http-edge',
+  Array   $http_edge_redirect_types     = [ 'beg', 'dir', 'reg', 'str' ],
+  Hash    $http_edge_domains_envs = {},
 ) {
 
   if $frontend {
-    create_resources('haproxy::frontend', $frontends, $frontend_defaults)
+
+    if $http_edge {
+
+      $rules = $http_edge_domains_envs.map |$key,$value| {
+        $value.map |$env,$domain| {
+          $http_edge_redirect_types.map |$style| {
+            "redirect location %[path,map_str(${http_edge_path}/${env}/current/redirects/${key}.${style}.map)] code 301 if { hdr(host) -i ${domain} } { path,map_str(${http_edge_path}/${env}/current/redirects/${key}.${style}.map) -m found }"
+          }
+        }
+      }
+
+      $http_request_rules = {'default_fe'=> {'options'=> {'http-request'=> concat($frontends[default_fe][options][http-request],flatten($rules))}}}
+
+      $frontends_real = deep_merge($frontends, $http_request_rules)
+
+      create_resources('haproxy::frontend', $frontends_real, $frontend_defaults)
+
+    } else {
+      create_resources('haproxy::frontend', $frontends, $frontend_defaults)
+    }
+
   }
 
   if $backend {
@@ -34,4 +60,3 @@ class ducktape::haproxy::autoload (
     create_resources('haproxy::userlist', $userlists, $userlist_defaults)
   }
 }
-
