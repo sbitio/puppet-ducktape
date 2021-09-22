1c8979be17e22ad408f1a2eacb2dcf3d35bfd814
kris.magjsitari <kris@sbit.io>
2021-09-22 11:38:44 +0200

    haproxy: Create http-edge directories if they dont exist

--- manifests/haproxy/autoload.pp
+++ manifests/haproxy/autoload.pp
@@ -10,36 +10,58 @@ class ducktape::haproxy::autoload (
   Boolean $userlist           = true,
   Hash    $userlist_defaults  = {},
   Hash    $userlists          = {},
-
-  Boolean $http_edge          = true,
-  String  $http_edge_frontend = 'default_fe',
-  String  $http_edge_path     = '/opt/http-edge',
-  Array   $http_edge_redirect_types     = [ 'beg', 'dir', 'reg', 'str' ],
-  Hash    $http_edge_domains_envs = {},
+  Boolean $http_edge                = true,
+  String  $http_edge_path           = '/opt/http-edge',
+  String  $http_edge_path_owner     = 'deploy',
+  String  $http_edge_frontend       = 'default_fe',
+  Array   $http_edge_redirect_types = [ 'str', 'beg', 'end', 'sub', 'dir', 'regm' ],
+  Hash    $http_edge_domains_envs   = {},
 ) {
 
   if $frontend {
 
     if $http_edge {
-
-      $rules = $http_edge_domains_envs.map |$key,$value| {
-        $value.map |$env,$domain| {
-          $http_edge_redirect_types.map |$style| {
-            "redirect location %[path,map_str(${http_edge_path}/${env}/current/redirects/${key}.${style}.map)] code 301 if { hdr(host) -i ${domain} } { path,map_str(${http_edge_path}/${env}/current/redirects/${key}.${style}.map) -m found }"
+      file {$http_edge_path:
+        ensure => directory,
+        owner => $http_edge_path_owner,
+        mode => "2755"
+      }
+      $http_edge_domains_envs.each |$key,$value| {
+        $value.each |$env,$domain| {
+          $path_exists = find_file("${http_edge_path}/${env}")
+          unless $path_exists {
+            file { ["${http_edge_path}/${env}",
+                    "${http_edge_path}/${env}/current",
+                    "${http_edge_path}/${env}/current/redirects",]:
+              ensure => directory,
+              owner => $http_edge_path_owner,
+              replace => false
+            }
+          }
+          $http_edge_redirect_types.each |$type| {
+            file {"${http_edge_path}/${env}/current/redirects/${key}.${type}.map":
+              ensure => present,
+              owner => $http_edge_path_owner,
+              replace => false
+            }
           }
         }
       }
-
-      $http_request_rules = {'default_fe'=> {'options'=> {'http-request'=> concat($frontends[default_fe][options][http-request],flatten($rules))}}}
-
+      $rules = $http_edge_domains_envs.map |$key,$value| {
+        $value.map |$env,$domain| {
+          $http_edge_redirect_types.map |$type| {
+            $http_edge_real_path = "${http_edge_path}/${env}/current/redirects/${key}.${type}.map"
+            "redirect location %[path,map_${type}(${http_edge_real_path})] code 301 if { hdr(host) -i ${domain} } { path,map_${type}(${http_edge_real_path}) -m found } !{ path,map_${type}(${http_edge_real_path}) -m str haproxy-skip }"
+          }
+        }
+      }
+      $http_request_rules = {$http_edge_frontend => {'options'=> {'http-request'=> concat($frontends[$http_edge_frontend][options][http-request],flatten($rules))}}}
       $frontends_real = deep_merge($frontends, $http_request_rules)
-
-      create_resources('haproxy::frontend', $frontends_real, $frontend_defaults)
-
     } else {
-      create_resources('haproxy::frontend', $frontends, $frontend_defaults)
+      $frontends_real = $frontends
     }
 
+    create_resources('haproxy::frontend', $frontends_real, $frontend_defaults)
   }
 
   if $backend {
