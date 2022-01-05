347fbd56b4ebcabf5ca6145c5b2a9c2cdca1228b
kris <kris@sbit.io>
2022-01-05 16:13:18 +0100

    posix_acl-module: Modernize ducktape

--- manifests/posix_acl.pp
+++ manifests/posix_acl.pp
@@ -1,13 +1,12 @@
 class ducktape::posix_acl (
-  $enabled = true,
+  Boolean $enabled = true,
+  Hash $defaults = {},
+  Hash $acls = {},
 ) {
-
-  validate_bool($enabled)
 
   if $enabled {
 
-    $posix_acl_defaults = hiera('ducktape::posix_acl::defaults', {})
-    create_resources('posix_acl', hiera_hash('ducktape::posix_acls', {}), $posix_acl_defaults)
+    require ::ducktape::posix_acl::autoload
 
   }
 
