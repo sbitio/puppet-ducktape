590eb8e8a339f52aa7ae1752a35ce4c971db10a8
kris <kris@sbit.io>
2022-01-10 14:09:21 +0100

    archive: Modernize ducktape

--- manifests/archive.pp
+++ manifests/archive.pp
@@ -1,15 +1,15 @@
 class ducktape::archive (
   $enabled = true,
+  Hash $defaults = {},
+  Hash $archives = {},
 ) {
 
   validate_bool($enabled)
 
   if $enabled {
 
-    $archive_defaults = hiera('ducktape::archive::defaults', {})
-    create_resources('archive', hiera_hash('ducktape::archives', {}), $archive_defaults)
+    require ducktape::archive::autoload
 
   }
 
 }
-
