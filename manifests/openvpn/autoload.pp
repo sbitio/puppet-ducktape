825bb5a0b104b4b42facdf8d4109f3e518c7d82f
kris <kris@sbit.io>
2022-01-11 14:01:39 +0100

    openvpn: modernize ducktape

--- manifests/openvpn/autoload.pp
+++ manifests/openvpn/autoload.pp
@@ -1,27 +1,19 @@
 class ducktape::openvpn::autoload (
-  $load_servers = true,
-  $load_clients = true,
-  $load_revokes = true,
+  Boolean $load_servers = true,
+  Boolean $load_clients = true,
+  Boolean $load_revokes = true,
 ) {
 
-  validate_bool($load_servers)
-  validate_bool($load_clients)
-  validate_bool($load_revokes)
-
   if $load_servers {
-    $server_defaults = hiera('ducktape::openvpn::server::defaults', {})
-    create_resources('::openvpn::server', hiera_hash('ducktape::openvpn::servers', {}), $server_defaults)
+    create_resources('::openvpn::server', $ducktape::openvpn::servers, $ducktape::openvpn::server_defaults)
   }
 
   if $load_clients {
-    $client_defaults = hiera('ducktape::openvpn::client::defaults', {})
-    create_resources('::openvpn::client', hiera_hash('ducktape::openvpn::clients', {}), $client_defaults)
+    create_resources('::openvpn::client', $ducktape::openvpn::clients, $ducktape::openvpn::client_defaults)
   }
 
   if $load_revokes {
-    $revoke_defaults = hiera('ducktape::openvpn::revoke::defaults', {})
-    create_resources('::openvpn::revoke', hiera_hash('ducktape::openvpn::revokes', {}), $revoke_defaults)
+    create_resources('::openvpn::revoke', $ducktape::openvpn::revokes, $ducktape::openvpn::revoke_defaults)
   }
 
 }
-
