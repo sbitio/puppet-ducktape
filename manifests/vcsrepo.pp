ed7783db89075f50f96fb4c4fc62d6af4a2280e1
kris <kris@sbit.io>
2022-01-11 14:18:15 +0100

    vcsrepo: modernize ducktape

--- manifests/vcsrepo.pp
+++ manifests/vcsrepo.pp
@@ -1,15 +1,11 @@
 class ducktape::vcsrepo (
-  $enabled = true,
+  Boolean $enabled = true,
+  Hash $repo_defaults = {},
+  Hash $repos = {},
 ) {
 
-  validate_bool($enabled)
-
   if $enabled {
-
-    $vcsrepo_defaults = hiera('ducktape::vcsrepo::defaults', {})
-    create_resources('vcsrepo', hiera_hash('ducktape::vcsrepos', {}), $vcsrepo_defaults)
-
+    require ::ducktape::vcsrepo::autoload
   }
 
 }
-
---
+++ manifests/vcsrepo/autoload.pp
@@ -0,0 +1,8 @@
+class ducktape::vcsrepo::autoload (
+  Boolean $autoload = true,
+) {
+
+  if $autoload {
+    create_resources('vcsrepo', $ducktape::vcsrepo::repos, $ducktape::vcsrepo::repo_defaults)
+  }
+}
