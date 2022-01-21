336538c9574931a3791cd21dd795e5d738ea6170
Jonathan Araña Cruz <jonhattan@faita.net>
2022-01-21 18:37:25 +0100

    Remove unused class ducktape::mysql::flavour

--- manifests/mysql.pp
+++ manifests/mysql.pp
@@ -1,12 +1,5 @@
 class ducktape::mysql (
   Boolean $enabled = true,
-  Optional[String] $flavour = undef,
 ) {
-
-  if $enabled {
-    class { '::ducktape::mysql::flavour' :
-      flavour => $flavour,
-    }
-  }
 
 }
--- manifests/mysql/client.pp
+++ manifests/mysql/client.pp
@@ -1,9 +1,2 @@
 class ducktape::mysql::client inherits ducktape::mysql {
-  if $ducktape::mysql::enabled {
-    anchor { 'ducktape::mysql::client::begin': } ->
-      Class['::ducktape::mysql::flavour'] ->
-      Class['::mysql::client'] ->
-    anchor { 'ducktape::mysql::client::end': }
-  }
 }
-
