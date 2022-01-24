1e4de94558e8ba9969ff9aefc85da2f40a62e181
kris <kris@sbit.io>
2022-01-24 10:49:26 +0100

    docker-compose: modernize ducktape

--- manifests/docker/compose.pp
+++ manifests/docker/compose.pp
@@ -4,8 +4,7 @@ class ducktape::docker::compose (
 
   if $enabled {
 
-    $docker_compose_defaults = hiera('ducktape::docker::compose::defaults', {})
-    create_resources('docker_compose', hiera_hash('ducktape::docker::compose', {}), $docker_compose_defaults)
+    create_resources('docker_compose', $ducktape::docker::compose, $ducktape::docker::compose_defaults)
 
   }
 
