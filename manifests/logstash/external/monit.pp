58d5591c1e148bd3ab976a7559a5818b934db325
Pedro González Serrano <niteman.es@gmail.com>
2023-03-17 14:04:47 +0100

    logstash/monit: Update binary&matching

--- manifests/logstash/external/monit.pp
+++ manifests/logstash/external/monit.pp
@@ -1,5 +1,7 @@
 class ducktape::logstash::external::monit(
   Boolean $enabled  = true,
+  String[1] $binary = '/usr/share/logstash/jdk/bin/java',
+  String[1] $matching = $binary,
   Array[Hash] $tests = [],
   Hash $restart_limit = $ducktape::monit_restart_limit,
 ) {
@@ -7,8 +9,8 @@ class ducktape::logstash::external::monit(
   if $enabled {
 
     monit::check::service { 'logstash':
-      binary        => '/usr/bin/java',
-      matching      => 'logstash',
+      binary        => $binary,
+      matching      => $matching,
       systemd_file  => '/etc/systemd/system/logstash.service',
       tests         => $tests,
       restart_limit => $restart_limit,
