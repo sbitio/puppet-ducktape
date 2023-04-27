6e9955ea9fb3aa0e8337c5d21863ea78102cfe73
Pedro González Serrano <niteman.es@gmail.com>
2023-04-27 19:39:06 +0200

    openldap: Remove resource autoloading
    Since current module's version (10.3.0) can handle hiera resource creation on tis own

--- manifests/openvpn.pp
+++ manifests/openvpn.pp
@@ -1,16 +1,8 @@
 class ducktape::openvpn (
   Boolean $enabled  = true,
-  Hash $server_defaults = {},
-  Hash $servers = {},
-  Hash $client_defaults = {},
-  Hash $clients = {},
-  Hash $revoke_defaults = {},
-  Hash $revokes = {},
 ) {
 
   if $enabled {
-    include ::ducktape::openvpn::autoload
-
     # External checks.
     if defined('::monit') and defined(Class['::monit']) {
       #TODO# BROKEN, pidfile depends on defines, cannotuse service check include ::ducktape::openvpn::external::monit
