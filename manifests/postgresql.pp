6ff1507574ecc792e0f0cefc3d3327d62fac382a
Jonathan Araña Cruz <jonhattan@faita.net>
2023-05-07 09:54:38 +0200

    postgresql: Add munin tests

--- manifests/postgresql.pp
+++ manifests/postgresql.pp
@@ -7,6 +7,9 @@ class ducktape::postgresql(
     if defined('::monit') and defined(Class['::monit']) {
       include ::ducktape::postgresql::external::monit
     }
+    if defined('::munin::node') and defined(Class['::munin::node']) {
+      include ::ducktape::postgresql::external::munin_node_plugin
+    }
   }
 
 }
